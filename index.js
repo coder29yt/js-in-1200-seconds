cb3509709512f814e314424d50c58c910032fcd2
coder29yt <134164127+coder29yt@users.noreply.github.com>
2023-06-02 03:56:46 +0530

    Uploaded Notes

---
+++ index.js
@@ -0,0 +1,61 @@
+// [Basic Syntax and Variables] // 1st topic
+
+
+// let, const ,var -- used to define variables 
+
+// let name = "Raj";
+// console.log(name)
+// const name2 = "Raj 2";
+// let name;
+// name = "Raj Padval";
+// console.log(name);
+
+
+
+// [Conditionals and Loops] //2nd topic
+
+
+
+// let age = 19;
+
+// if (age >= 18) {
+//   console.log("You can drive the car");
+// } else {
+//   console.log("You can't drive the car");
+// }
+
+// for (let i = 0; i <= 9; i++) {
+//   console.log("You are a good boy");
+// }
+
+
+
+
+// Function // last topic
+
+
+
+// function subscribe(name) {
+//   console.log(`${name} subscribe to my channel`);
+// }
+// subscribe("Riddhi");
+
+
+
+
+// [DOM Manipulation] //3rd topic
+
+
+// const text = document.querySelector("h1")
+// text.textContent = "Good Night"
+
+
+
+// [Event Handling] //4tth topic
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const button = document.getElementById("subBtn");
+
+// button.addEventListener("click", function () {
+//   console.log(`Email : ${email.value}, Password : ${password.value}`);
+// });
